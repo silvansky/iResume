77537aaf8ab8e6b2d21b4560c999b10e1cf726d1
silvansky <v.silvansky@gmail.com>
2012-07-30 15:26:43 +0400

    Initial Commit

---
+++ iResume/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iResume
+//
+//  Created by Valentine on 30.07.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
